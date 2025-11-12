051054c4670ecb2edf9e5e58fc9c8f6e4ef8ff87
Huda Khan <hudakhan122211@gmail.com>
2025-11-12 20:04:51 +0500

    function added

---
+++ JS Function/app.js
@@ -0,0 +1,41 @@
+// function studentData(a){
+//     console.log(`${a} is Present`)
+// }
+// studentData("Ali")
+// studentData("Ahmad")
+// studentData("Hamza")
+// studentData("Huda")
+// studentData("Farhana")
+
+// function data(val1, val2){
+//     console.log(val1 + val2)
+// }
+// data(1, 2)
+// data(1, 2)
+// data(1, 2)
+// data(1, 2)
+// data(1, 2)
+// data(1, 2)
+// data(1, 2)
+
+// arrow function
+// let data = (a , b)=>{
+//     console.log(a+b);
+// }
+// data(2,2)
+
+// let myfun = function myData(a , b){
+//     console.log(a + b);  
+// }
+// myData(2,2)
+
+// let data = (...v1)=>{
+//     console.log(v1);
+// }
+// data(1,2,3,4,5,6,7,8,9,10)
+
+
+// let data = (a = 0 , b = 0)=>{
+//     console.log(a+b);
+// }
+// data()
