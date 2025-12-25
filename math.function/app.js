4bbb7f30d4883f99b1e2bcb85f3dbc9e43450e39
Huda Khan <hudakhan122211@gmail.com>
2025-12-25 19:42:11 +0500

    New work

---
+++ math.function/app.js
@@ -0,0 +1,38 @@
+// let a = Math.floor(Math.random()*11)
+// console.log(a)
+
+
+
+
+
+// function otp() {
+//     let otp = Math.floor(1000 + Math.random() * 9000);
+//     // console.log(otp);
+//     alert(otp)
+// }
+
+// function otp() {
+//     let otp = document.getElementById("otp-text")
+//     otp.innerHTML = otp
+// }
+
+
+
+// function otp() {
+//     let otpBox = document.getElementById("otp-text");
+//     let randomOTP = Math.floor(1000 + Math.random() * 9000);
+//     let OTP = otpBox.innerHTML = randomOTP;
+//     alert(OTP)
+// }
+
+// function otp(){
+//     let otpBox = document.getElementById("otp-text");
+//     let randomOTP = Math.floor(1000 + Math.random()* 9000);
+    
+//     alert(randomOTP);
+// }
+
+
+    let text = document.getElementById("new") ;
+    // console.log(text);
+ text.innerHTML= "new text is here"
